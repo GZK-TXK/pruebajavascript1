ce77f382345e6f97d4f5cb563b046682d6c15db1
Gaizka <chocarrovaz.gaizka@gmail.com>
2026-04-29 13:48:43 +0200

    Ejercicios finalizados, falta ultima revision

---
+++ JS/main.js
@@ -0,0 +1,43 @@
+//////////////////////////////////////////////////////////////////////////////////////////////
+//EJERCICIO 1
+//Declara una función que acepte una palabra como parámetro y la devuelva con todos sus caracteres en mayúsculas y separados por espacios. Ejemplo: Para "Ana" devolverá "A N A".
+
+const nombre= "ana";
+
+function dividirNombre(palabra){
+    return palabra.split("").join(" ").toUpperCase();
+}
+
+const resultado = dividirNombre(nombre);
+
+console.log(resultado);
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+//EJERCICIO 2
+//Declara una función que acepte un nombre como parámetro y devuelva el número de palabras que tiene el nombre. Ejemplo: Para "Antonio Alberto Jesús" devolverá 3.
+
+const nombre0= "Fernando Alonso Díaz Garcia";
+
+let arrayNombre= nombre0.split(" ");
+
+let resultado1 = arrayNombre.length;
+
+console.log(resultado1);
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+//EJERCICIO 3
+//Diseña un algoritmo que cuente las veces que aparece una determinada letra en una frase.
+
+const frase= "Ejercicio de practica";
+
+let letra0= "e";
+
+let arrayFrase= frase.split("");
+
+let letrica= arrayFrase.filter(function(letra){
+    return letra == letra0
+});
+
+let resultado2= letrica.length;
+
+console.log(resultado2)
